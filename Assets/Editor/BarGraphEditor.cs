25dcf1f101a2d6d760ef343a3158f0b030302fb6
linusgaertig <linus.gaertig@gmail.com>
2020-07-03 22:49:47 +0200

    started working on graphs

---
+++ Assets/Editor/BarGraphEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(BarGraph))]
+public class LevelScriptEditor : Editor {
+    public override void OnInspectorGUI() {
+
+        DrawDefaultInspector();
+
+        BarGraph myTarget = (BarGraph)target;
+
+        if (GUILayout.Button("Add Graph")) {
+            myTarget.AddGraph();
+        }
+    }
+}
---
+++ Assets/Scripts/Bar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Bar
+{
+    public float value = 0;
+    public string parameter = "";
+
+    public Bar(float newValue, string newParameter) {
+        value = newValue;
+        parameter = newParameter;
+    }
+}
