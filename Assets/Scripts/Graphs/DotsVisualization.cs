a7219ad120c9f01711771c623b8c00a8afc54fd7
linusgaertig <linus.gaertig@gmail.com>
2020-07-04 14:04:38 +0200

    added rewindable dots and lines

--- Assets/Scripts/Graphs/DotsVisualization.cs
+++ Assets/Scripts/Graphs/DotsVisualization.cs
@@ -11,14 +11,21 @@ public class DotsVisualization : MonoBehaviour
     int count = 0;
     float curStep = 0;
 
+    //latest values for rewind
+    float lastValue = 0;
+    float curLastValue = 0;
+
     [Header("References")]
     public GameObject dotPrefab = default;
     public List<GameObject> curDots = default;
 
-    void Update()
-    {
+    void Update() {
         var curStatistic = Statistics.instance.Get(vector2Statistic);
+        foreach (var position in curStatistic) {
+            curLastValue = position.x;
+        }
 
+        DeleteDotsOnRewind();
 
         foreach (var position in curStatistic) {
             if (position.x >= curStep) {
@@ -29,9 +36,28 @@ public class DotsVisualization : MonoBehaviour
                 newDot.transform.SetParent(this.transform);
                 curDots.Add(newDot);
 
+                lastValue = position.x;
                 curStep += stepSize;
                 count += 1;
             }
+        }
+    }
+
+    private void DeleteDotsOnRewind() {
+        if (curLastValue < lastValue) {
+            List<GameObject> deleteDots = new List<GameObject>();
+            foreach (GameObject dot in curDots) {
+                if (dot.transform.position.x > curLastValue) {
+                    deleteDots.Add(dot);
+                    curStep -= stepSize;
+                }
+            }
+
+            foreach (GameObject dot in deleteDots) {
+                curDots.Remove(dot);
+                GameObject.Destroy(dot);
+            }
+            deleteDots.Clear();
         }
     }
 }
--- Assets/Scripts/Graphs/LineVisualization.cs
+++ Assets/Scripts/Graphs/LineVisualization.cs
@@ -11,12 +11,21 @@ public class LineVisualization : MonoBehaviour
     int count = 0;
     float curStep = 0;
 
+    //latest values for rewind
+    float lastValue = 0;
+    float curLastValue = 0;
+
     [Header("References")]
     public LineRenderer lineRenderer = default;
 
     void Update() {
         var curStatistic = Statistics.instance.Get(vector2Statistic);
-        
+
+        foreach (var position in curStatistic) {
+            curLastValue = position.x;
+        }
+
+        DeletePostionsOnRewind();
 
         foreach (var position in curStatistic) {
             if (position.x >= curStep) {
@@ -25,10 +34,21 @@ public class LineVisualization : MonoBehaviour
                 Vector3 newPos = new Vector3(this.transform.position.x + position.x, this.transform.position.y + position.y, 0);
                 //print(newPos);
                 lineRenderer.SetPosition(count, newPos);
+                lastValue = position.x;
                 curStep += stepSize;
                 count += 1;
             }
         }
         
+    }
+
+    private void DeletePostionsOnRewind() {
+        if (curLastValue < lastValue) {
+            if (lineRenderer.GetPosition(lineRenderer.positionCount - 1).x > curLastValue) {
+                count -= 1;
+                curStep -= stepSize;
+                lineRenderer.positionCount = count;
+            }
+        }
     }
 }
