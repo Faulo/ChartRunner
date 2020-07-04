267da675445d73e5af10157976a1bbc436ce6b39
linusgaertig <linus.gaertig@gmail.com>
2020-07-04 20:59:09 +0200

    added camera shake

---
+++ Assets/Scripts/Effects/ShakeImpulseManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class ShakeImpulseManager : MonoBehaviour
+{
+    public CinemachineImpulseSource softImpulse = default;
+    public CinemachineImpulseSource mildImpulse = default;
+    public CinemachineImpulseSource strongImpulse = default;
+
+    public static ShakeImpulseManager instance = default;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void FireImpulse(ImpulseStrength impulse) {
+        switch (impulse) {
+            case ImpulseStrength.softImpulse:
+                softImpulse.GenerateImpulse();
+                break;
+            case ImpulseStrength.mildImpulse:
+                mildImpulse.GenerateImpulse();
+                break;
+            case ImpulseStrength.strongImpulse:
+                strongImpulse.GenerateImpulse();
+                break;
+            default:
+                softImpulse.GenerateImpulse();
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/ScriptableObjects/PostProcessing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PostProcessing", menuName = "ScriptableObjects/PostProcessing", order = 1)]
+public class PostProcessing : ScriptableObject {
+    public float newChromaticAberation = 0;
+    public float newLensDisortion = 0;
+    public void ChromaticAberration() {
+        PostProcessingManager.instance.ChangeChromaticAberration(newChromaticAberation);
+    }
+
+    public void LensDisortion() {
+        PostProcessingManager.instance.ChangeLensDisortion(newLensDisortion);
+    }
+}
