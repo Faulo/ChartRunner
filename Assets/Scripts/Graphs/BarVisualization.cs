5a784cfe0a9fcd8c500d2b1bed05d2db19221dde
linusgaertig <linus.gaertig@gmail.com>
2020-07-04 16:25:43 +0200

    adjusted bar classes

--- Assets/Scripts/Graphs/BarVisualization.cs
+++ Assets/Scripts/Graphs/BarVisualization.cs
@@ -8,22 +8,18 @@ public class BarVisualization : MonoBehaviour
     public float value = 0;
     public FloatStatistic statistic = default;
 
-    float yOffset = 0.256f;
-    float yPos = 0f;
+    [Tooltip("Y Wert steht fuer parameter per Value")]
+    public Vector3 size = new Vector3(1f, 1f, 1f);
 
     Transform parent = default;
     private void Start() {
         parent = GetComponentInParent<Transform>();
-        yPos = parent.transform.position.y;
     }
 
     void Update() {
         value = Statistics.instance.Get(statistic);
 
         // magic number 0.256 pro 0.1 Verschiebung nach oben
-        this.transform.localScale = new Vector3(1f, value, 1f);
-
-        
-        this.transform.position = new Vector3(this.transform.position.x, yPos, 0);
+        this.transform.localScale = new Vector3(size.x, value * size.y, size.z);
     }
 }
