03569e852e964c9548e434bbe84a88d8b8f8b838
Ivette Schmidt <ivette_schmidt@outlook.com>
2020-07-05 14:08:53 +0200

    added basis for adding statistics to sheet

---
+++ Assets/Scripts/Statistics2Sheet.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Slothsoft.UnityExtensions;
+using TMPro;
+using UnityEngine;
+
+public class Statistics2Sheet : MonoBehaviour {
+    [SerializeField, Expandable]
+    TextMeshProUGUI textMesh = default;
+
+    [SerializeField, Expandable]
+    List<TextMeshProUGUI> names;
+    [SerializeField, Expandable]
+    List<TextMeshProUGUI> values;
+
+    Dictionary<string, string> namevaluepairs = new Dictionary<string, string>();
+
+    Regex digit = new Regex(@"\d+");
+    Regex upper = new Regex(@"^|\b[A-Z]+[a-z]+\b");
+
+    void Split() {
+        string raw = Statistics.instance.statusText;
+        
+        string[] words = Regex.Split(raw, @"\s+");
+
+        bool namevalueswitch = true;
+        int counterUpper = 0;
+        int counterDigits = 0;
+
+        foreach (string word in words) {
+            //LOGIC
+            var matchDigit = digit.Match(word);
+            var matchUpper = upper.Match(word);
+            if (matchDigit.Success && namevalueswitch && counterDigits < values.Capacity ) {
+                values[counterDigits].text = Regex.Replace(word, @"[\|\:()]", " ");
+                counterDigits += 1;
+                namevalueswitch = false;
+                //Debug.Log(word);
+            } else if (matchUpper.Success && counterUpper < names.Capacity) {
+                names[counterUpper].text = Regex.Replace(word, @"[\:()]", " ");
+                counterUpper += 1;
+                namevalueswitch = true;
+                //Debug.Log(word);
+            }
+            
+        }
+    }
+
+    void Update() {
+        Split();
+        
+        textMesh.text = Statistics.instance.statusText;
+    }
+
+    void OnValidate() {
+        if (!textMesh) {
+            textMesh = GetComponentInParent<TextMeshProUGUI>();
+        }
+    }
+}
+
